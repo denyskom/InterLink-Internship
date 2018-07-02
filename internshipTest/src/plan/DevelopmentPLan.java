4568247f93e67c8ddd9a113ed22518c943ee6151
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-07-02 17:49:17 +0300

    Development plan first implementation

---
+++ internshipTest/src/plan/DevelopmentPLan.java
@@ -0,0 +1,10 @@
+package plan;
+
+import institution.KnowledgeSource;
+import person.Student;
+
+import java.util.List;
+
+public class DevelopmentPLan {
+    private List<KnowledgeSource> knowledgeSources;
+}
---
+++ internshipTest/src/plan/Schedule.java
@@ -0,0 +1,10 @@
+package plan;
+
+import java.time.LocalTime;
+
+public class Schedule {
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+
+}
